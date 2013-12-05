bd8832937cf820f0e9f30b3078e6209e1b19e1a2
Joe (WORK) <joseph.parker.1@gmail.com>
2013-12-05 16:55:52 -0500

    using logic and macros to deduce possible functions for arg sets

--- src/ship/logic.clj
+++ src/ship/logic.clj
@@ -251,10 +251,179 @@ per
 (defrel creates-literal f a)
 
 
-;literal
-;function
-;start-seq
-;end-seq
+
+
+;:TODO (factualize in-vars, so they can be permutated)
+
+;algorithm for some basic permutations of function / arg arrangement
+
+;use logic for transformations (in -> out)
+
+
+
+
+
+
+(defrel number! n)
+(defrel string! n)
+(defrel nil! n)
+(defrel function! n)
+(defrel collection! n)
+
+(defrel type! n)
+(defrel literal! n)
+
+;we want to record function relations with the arity count and types
+;this can be generated
+(defrel arity-type-first! f a)
+(defrel arity-type-rest! f a )
+(defrel return-type! f a )
+
+(defrel arity-req-more! f)
+
+(defn same-type [a b]
+  (conde
+   [(number! a)(number! b)(type! b)(literal! a)]
+   [(string! a)(string! b)(type! b)(literal! a)]
+   [(collection! a)(collection! b)(type! b)(literal! a)]))
+
+(defn not-needs-more [a]
+  (fresh [h]
+     (arity-req-more! h)
+     (!= a h)))
+
+;lets assume functions have first and rest conditions for arity type
+
+(facts arity-type-first! [['+ :number]
+                        ['- :number]
+                        ['/ :number]
+                        ['* :number]
+                        ['inc :number]
+                        ['dec :number]
+                        ['str :number] ['str :string]
+
+                        ['interleave :string]['interleave :coll]
+
+                        ['cons :number]['cons :string]['cons :coll]
+                        ['conj :coll]
+
+                        ['count :string]['count :coll] ])
+(facts arity-type-rest! [['+ :number]
+                        ['- :number]
+                        ['/ :number]
+                        ['* :number]
+                        ['str :number] ['str :string]
+                        ['max :number]
+                        ['min :number]
+                        ['interleave :string]['interleave :coll]
+
+                        ['cons :coll]
+                        ['conj :number]['conj :string]['conj :coll]])
+
+(facts return-type!    [['+ :number]
+                        ['- :number]
+                        ['/ :number]
+                        ['* :number]
+                        ['str :string]
+                        ['inc :number]
+                        ['dec :number]
+                        ['count :number]
+                        ['interleave :coll]
+                        ['cons :coll]
+                        ['conj :coll]])
+
+
+(facts arity-req-more! [['interleave]['cons]['conj]])
+
+
+(run 20 [q] (arity-type-first! q :string)(not-needs-more q))
+
+
+(fact number! :number)
+(fact string! :string)
+(fact nil! :nil)
+(fact collection! :coll)
+
+
+(facts type! [[:number][:string][:nil][:coll]])
+
+(def in-vars ['in-a 'in-b 'in-c 'in-d 'in-e 'in-f 'in-g 'in-h])
+
+
+;;
+
+(defn get-arity
+  ([f1 a]
+  (fresh [v]
+      (same-type a v)
+      (arity-type-first! f1 v)
+      (not-needs-more f1)))
+  ([f1 a & more]
+  (fresh [v k]
+      (same-type a k)
+      (arity-type-first! f1 k)
+      (same-type (first more) v)
+      (arity-type-rest! f1 v)))
+  )
+
+
+;;
+(defn conf-head [vars]
+  (str "(run 100 [q] (fresh [f1 " vars "] ") )
+(defn conf-tail [vars in]
+  (str "(get-arity f1 " (string/join " " vars ) ")  (== q `(~f1 ~" (string/join " ~" vars ) ")) ))" ))
+
+(defn conf-in-facts [two]
+  "makes type facts from the in arg literals"
+  (let [in (first two)
+        var (last two)
+        literal `(~'fact ~'literal! ~in)]
+  (cond
+   (number? in) (str `(~'fact ~'number! ~in) literal)
+   (string? in) (str `(~'fact ~'string! ~in) literal)
+   (coll? in) (str `(~'fact ~'collection! ~in) literal)
+   :else (str `(~'fact ~'nil! ~in) literal))))
+
+(defn conf-in-rel [two]
+  "makes goals from the in arg literals"
+  (let [in (first two)
+        var (last two)
+        literal `(~'literal! ~var)]
+  (cond
+   (number? in) (str `(~'number! ~var)`(~'literal! ~var))
+   (string? in) (str `(~'string! ~var)`(~'literal! ~var))
+   (coll? in) (str `(~'collection! ~var)`(~'literal! ~var))
+   :else (str `(~'nil! ~var)`(~'literal! ~var)))))
+
+(defn conflibulate [in out]
+  "returns a logic run for the in and out data"
+  (let [vars (take (count in) in-vars)]
+  (str (apply str (map conf-in-facts (reverse (zipmap in vars))))
+       (conf-head (string/join " " vars ))
+       (apply str (map conf-in-rel (reverse (zipmap in vars))))
+       (conf-tail vars (string/join " " in )))
+  ))
+
+
+
+
+
+(def flib (conflibulate [[1 44 9 ] "parker"] nil))
+flib
+
+(into #{} (load-string flib))
+(map eval (into #{} (load-string flib)))
+
+
+
+
+(fact number! 67)(fact literal! 67)(fact number! 5)(fact literal! 5)
+(run* [q] (fresh [f1 in-a in-b]
+                   (number! in-a)(literal! in-a)
+                   (number! in-b)(literal! in-b)
+
+                   (== q [in-a in-b] )))
+
 
 (defrel literal a)
 (defrel l-s a)
@@ -337,4 +506,10 @@ res
 (seq? [1 3])
 
 ((fn [n s col] (apply str col)) 1 "s" [1 2])
+
+
+
+
+
+
 
